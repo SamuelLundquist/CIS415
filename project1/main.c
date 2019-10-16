c336b9b454249cb515d8b1f84ee1d88b07e9016c
Samuel Lundquist <samzillard@Samuels-MacBook-Pro.local>
2019-10-16 15:36:57 -0700

    set up makefile

---
+++ project1/main.c
@@ -0,0 +1,18 @@
+/*
+* Description: Project 1 Main File.
+*
+* Author: Samuel Lundquist
+*
+* Date: 10/16/2019
+*
+* Notes:
+*
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include "command.h"
+
+int main(int argc, char *argv[])
+{
+	return 0;
+}
