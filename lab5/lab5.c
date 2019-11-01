4dd2475c1fc6d71c7363cb4793f835c80680364f
Samuel Lundquist <samzillard@Samuels-MacBook-Pro.local>
2019-11-01 09:54:23 -0700

    Yeet yah

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -12,6 +12,7 @@
 /*-------------------------Preprocessor Directives---------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 #include <dirent.h>
 #include <string.h>
 #include <unistd.h>
@@ -19,28 +20,60 @@
 /*---------------------------------------------------------------------------*/
 
 /*-----------------------------Program Main----------------------------------*/
+
+void handler(int signal)
+{
+	printf("    Child Process: %d - Recieved signal: %d", getpid(), signal);
+	sigset_t set;
+	sigemptyset(&set);
+	sigaddset(&set, SIGUSR1);
+	sigwait(&set, &signal);
+}
+
 int	 main()
 {
 	//variable definitions
 	pid_t  pid, w;
 	int wstatus, i;
 	i=0;
-	
+
+	struct sigaction action;
+	action.sa_handler = handler;
+	sigaction(SIGUSR1, &action, NULL);
+
 	pid = fork();
-	if (pid == 0) {
+	if(pid < 0)
+	{
+		perror("Error! could n ot create a new process.\n");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
 		printf("	Child Process: %i - Starting infinite loop...\n", getpid());
-		while(1) {
+		sleep(3);
+		/*
+		while(1)
+		{
 			i++;
-			if(i%10000) {
-			printf("	Child Process: %i - Running infinite loop...\n", getpid());
-			i=0
+			if(i%10000 == 0)
+			{
+				printf("	Child Process: %i - Running infinite loop...\n", getpid());
+				i=0;
 			}
 		}
-	} else {
+		*/
+		printf("    Child process: %d - Finished.\n", getpid());
+		exit(EXIT_SUCCESS);
+	}
+	else
+	{
 		//else this is an existing proc i.e. the parent
 		printf("Parent Process: %i, Waiting for child to finish...\n", getpid());
+		kill(SIGUSR1, pid);
 		w = waitpid(pid, &wstatus, WUNTRACED | WCONTINUED);
 		printf("All child processes joined. Exiting.\n");
 	}
+
+	return EXIT_SUCCESS;
 }
 /*-----------------------------Program End-----------------------------------*/
