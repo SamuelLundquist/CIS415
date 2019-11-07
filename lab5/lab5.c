12a4d3fff24339a2f5b13fddc288435f94cf32ec
Samuel Lundquist <samzillard@Samuels-MacBook-Pro.local>
2019-11-07 07:03:18 -0800

    yeet

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -23,11 +23,13 @@
 
 void handler(int signal)
 {
-	printf("    Child Process: %d - Recieved signal: %d", getpid(), signal);
+	printf("    Child Process: %d - Recieved signal: %d\n", getpid(), signal);
 	sigset_t set;
 	sigemptyset(&set);
 	sigaddset(&set, SIGUSR1);
-	sigwait(&set, &signal);
+	sigprocmask(SIG_BLOCK, &set, NULL);
+	int sig;
+	sigwait(&set, &sig);
 }
 
 int	 main()
@@ -39,40 +41,37 @@ int	 main()
 
 	struct sigaction action;
 	action.sa_handler = handler;
-	sigaction(SIGUSR1, &action, NULL);
+	sigemptyset(&action.sa_mask);
 
 	pid = fork();
 	if(pid < 0)
 	{
-		perror("Error! could n ot create a new process.\n");
+		perror("Error! could not create a new process.\n");
 		exit(EXIT_FAILURE);
 	}
 	else if (pid == 0)
 	{
 		printf("	Child Process: %i - Starting infinite loop...\n", getpid());
-		sleep(3);
-		/*
+		//sleep(3);
+		sigaction(SIGUSR1, &action, NULL);
 		while(1)
 		{
-			i++;
-			if(i%10000 == 0)
-			{
-				printf("	Child Process: %i - Running infinite loop...\n", getpid());
-				i=0;
-			}
+			printf("	Child Process: %i - Starting infinite loop...\n", getpid());
+			sleep(1);
 		}
-		*/
-		printf("    Child process: %d - Finished.\n", getpid());
 		exit(EXIT_SUCCESS);
 	}
 	else
 	{
-		//else this is an existing proc i.e. the parent
-		printf("Parent Process: %i, Waiting for child to finish...\n", getpid());
-		kill(SIGUSR1, pid);
-		w = waitpid(pid, &wstatus, WUNTRACED | WCONTINUED);
-		printf("All child processes joined. Exiting.\n");
+
 	}
+	kill(pid, SIGUSR1);
+	sleep(4);
+	kill(pid, SIGUSR1);
+	sleep(2);
+	//kill(pid, SIGQUIT);
+	printf("	WAITING\n");
+	w = waitpid(pid, &wstatus, WUNTRACED | WCONTINUED);
 
 	return EXIT_SUCCESS;
 }
---
+++ project2/part2.c
@@ -0,0 +1,214 @@
+/*
+* Description: Project 2 Part 2.
+*
+* Author: Samuel Lundquist
+*
+* Date: 11/6/2019
+*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+char **malloc2DArr(int num, int char_size)
+{
+	char **new_arr = malloc(num * sizeof(char*));
+	for (int i = 0; i < num; i++)
+	{
+		new_arr[i] = malloc(char_size * sizeof(char));
+	}
+	return new_arr;
+}
+
+char **arrayFromString(char* s, char* delim, int index, int char_size)
+{
+	char* word;
+	if(index == 0)
+	{
+		word = strtok(s, delim);
+	}
+	else
+	{
+		word = strtok(NULL, delim);
+	}
+
+	if(word == NULL)
+	{
+
+		char** arr = malloc2DArr(index + 1, char_size);
+		free(arr[index]);
+		arr[index] = NULL;
+		return arr;
+	}
+	else
+	{
+		int len = strlen(word);
+		if(len > char_size)
+		{
+			char_size = len;
+		}
+		char** arr = arrayFromString(s, delim, index+1, char_size);
+		strcpy(arr[index], word);
+		return arr;
+	}
+}
+
+char ***getCommands(FILE* fp, int index)
+{
+	char delim[6] = " \n\r";
+	char* buf;
+	size_t bsize = 255;
+	int line;
+
+	buf = malloc(bsize * sizeof(char));
+	if(buf == NULL)
+	{
+		perror("Unable to allocate buffer.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	line = getline(&buf, &bsize, fp);
+	if(line == -1)
+	{
+		free(buf);
+		char ***arr = malloc((index + 1) * sizeof(char**));
+		free(arr[index]);
+		arr[index] = NULL;
+		return arr;
+	}
+	else
+	{
+		char **command = arrayFromString(buf, delim, 0, 0);
+		char ***arr = getCommands(fp, index+1);
+		arr[index] = command;
+		free(buf);
+		return arr;
+	}
+}
+
+void freeCommands(char ***arr)
+{
+	int i = 0;
+	int j = 0;
+
+	while(arr[i])
+	{
+		while(arr[i][j])
+		{
+			free(arr[i][j]);
+			j++;
+		}
+		j=0;
+		free(arr[i]);
+		i++;
+	}
+	free(arr);
+	return;
+}
+
+void handler(int signal)
+{
+	printf("    Child Process: %d - Recieved signal: %d", getpid(), signal);
+	raise(SIGCONT);
+}
+
+int main(int argc, char **argv)
+{
+
+	/* Check to make sure correct parameters are passed to main */
+	if (argc != 2)
+	{
+		printf("\n[ERROR] Too many parameters were passed to main.\n"
+			"This program takes a single filename as an input.\n\n");
+			exit(EXIT_FAILURE);
+	}
+
+	if (argv[1] == NULL)
+	{
+		printf("\n[ERROR] A filename was not given to main.\n"
+			"This program takes a single filename as an input.\n\n");
+			exit(EXIT_FAILURE);
+	}
+	/*************************************************************/
+
+	/* Open the file, handle error case */
+	FILE* read_file = fopen(argv[1], "r");
+	if(read_file == NULL)
+	{
+		perror("[ERROR] fopen()");
+		exit(EXIT_FAILURE);
+	}
+	/*************************************************************/
+
+	/* Get commands recursively from file and then print them to console */
+	char ***commands = getCommands(read_file, 0);
+	fclose(read_file);
+	int num_commands = 0;
+	int j = 0;
+	char *p;
+	char **q;
+	while((q = commands[num_commands]) != NULL)
+	{
+		while((p = q[j]) != NULL)
+		{
+			printf("%s ", p);
+			j++;
+		}
+		j = 0;
+		printf("\n");
+		num_commands++;
+	}
+	/*************************************************************/
+
+	/* Fork processes and run execvp() for each command in commands */
+	pid_t pids[num_commands];
+
+	for(int i = 0 ; i < num_commands ; i++)
+	{
+		pid_t pid;
+		if((pid = fork()) == 0)
+		{
+			printf("%d: [son] pid %d from [parent] pid %d\n", i, getpid(), getppid());
+			signal(SIGUSR1, handler);
+			//raise(SIGSTOP);
+			//raise(SIGCONT);
+			execvp(commands[i][0], commands[i]);
+			printf("[son] %d: execvp() failed.\n", getpid());
+			exit(EXIT_FAILURE);
+		}
+		if(pid < 0)
+		{
+			perror("Error! could not create a new process.\n");
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			pids[i] = pid;
+		}
+	}
+	/*************************************************************/
+
+	/*
+	for(int i = 0; i < num_commands; i++)
+	{
+		kill(pids[i], SIGUSR1);
+	}
+	*/
+
+	/* Wait for all forked processes to finish execution */
+	for(int i = 0; i < num_commands; i++)
+	{
+		printf("waiting\n");
+		wait(NULL);
+	}
+	/*************************************************************/
+
+	printf("Done waiting\n");
+
+	freeCommands(commands);
+
+	exit(EXIT_SUCCESS);
+}
