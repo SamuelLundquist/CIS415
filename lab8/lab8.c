2908e116c704f4ca6e1e7a60bbcac1be5c75d63d
Samuel Lundquist <samzillard@Samuels-MacBook-Pro.local>
2019-11-19 14:25:32 -0800

    added lab8

---
+++ lab8/lab8.c
@@ -0,0 +1,26 @@
+/*
+* Description: Lab 8
+*
+* Author: Samuel Lundquist
+*
+*/
+
+#define MAXNAME = 256;
+
+typedef struct{
+	int ticketNum;
+	char *dish;
+}mealTicket;
+
+typedef struct{
+	char *name[MAXNAME]
+	mealTicket *const buffer;
+	int head;
+	int tail;
+	const int length;
+}MTQ;
+
+int main()
+{
+	
+}
