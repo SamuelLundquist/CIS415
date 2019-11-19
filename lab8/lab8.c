f3f281e4da9c471a7ac604af1ca37e42a7693fbe
Samuel Lundquist <samzillard@Samuels-MacBook-Pro.local>
2019-11-19 15:00:42 -0800

    added lab8

--- lab8/lab8.c
+++ lab8/lab8.c
@@ -3,8 +3,10 @@
 *
 * Author: Samuel Lundquist
 *
+* Notes:
 */
 
+#define MAXQUEUES = 1;
 #define MAXNAME = 256;
 
 typedef struct{
@@ -13,14 +15,26 @@ typedef struct{
 }mealTicket;
 
 typedef struct{
-	char *name[MAXNAME]
+	char name[MAXNAME]
 	mealTicket *const buffer;
 	int head;
 	int tail;
 	const int length;
 }MTQ;
 
+MTQ registry[MAXQUEUES];
+
+int enqueue(char *MTQ_ID, mealTicket *MT)
+{
+	return 1;
+}
+
+int dequeue(char *MTQ_ID, int ticketNum, malTicket *MT)
+{
+	return 1;
+}
+
 int main()
 {
-	
+
 }
