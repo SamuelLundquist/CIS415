b172b8f4c458c71f1162c3812a35ab2d79f4d217
Samuel Lundquist <samzillard@Samuels-MacBook-Pro.local>
2019-12-05 01:22:56 -0800

    added dequeue functionality

--- project3/quacker.c
+++ project3/quacker.c
@@ -54,19 +54,36 @@ int enqueue(char *TQ_ID, topicEntry *TE)
 			ENTRYVAL++;
 			queue->buffer[t] = *TE;
 			queue->entries++;
-			if(t == queue->length)
-			{
-				queue->tail = 0;
-			}
-			else
-			{
-				queue->tail = t + 1;
-			}
+			t++;
+			queue->tail = t % (queue->length+1);
 			ret = 1;
 		}
 	}
 	return ret;
 }
+
+int dequeue(char *TQ_ID, int entryNum)
+{
+	TQ *queue;
+	int ret = 0;
+
+	if((queue = findQueue(TQ_ID)) != NULL)
+	{
+		int h = queue->head;
+		int t = queue->tail;
+		int entry = queue->buffer[h].entryNum;
+		if(entry >= 0 && entry == entryNum)
+		{
+			queue->buffer[t].entryNum = 0;
+			queue->buffer[h].entryNum = -1;
+			h++;
+			queue->head = h % queue->length;
+			ret = 1;
+		}
+	}
+	return ret;
+}
+
 /******************************************************************************/
 
 int main()
@@ -81,8 +98,16 @@ int main()
 		tree.photoURL, tree.photoCaption);
 
 	enqueue("Nature", &tree);
-	printf("%s:%d - %s\n", registry[0]->topic, registry[0]->entries,
-		registry[0]->buffer[0].photoURL);
+	enqueue("Nature", &tree);
+	enqueue("Nature", &tree);
+	enqueue("Nature", &tree);
+	enqueue("Nature", &tree);
+	enqueue("Nature", &tree);
+	for(int i = 0; i < 5; i++)
+	{
+		printf("%s:%d - %s\n", registry[0]->topic, registry[0]->entries,
+			registry[0]->buffer[i].photoURL);
+	}
 
 	atexit(exitStat);
 	return EXIT_SUCCESS;
